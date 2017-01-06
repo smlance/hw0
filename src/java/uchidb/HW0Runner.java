58db91ed8c41f56dde54186fe4f4384269228d14
Stefan Lance <slance@uchicago.edu>
2017-01-05 22:34:33 -0600

    Write initSet and initList

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -1,5 +1,9 @@
 package uchidb;
 
+import java.util.Set;
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @author aelmore
@@ -7,16 +11,29 @@ package uchidb;
 public class HW0Runner {
 
 	//TODO you likely will need to add member variable
-	
+    private static class MyCollections implements Containers<Integer, String> {
+	public Set<Integer> initSet(Integer[] tArray) {
+	    return new HashSet<Integer>(Arrays.asList(tArray));
+	}
+	public List<Integer> initList(Integer[] tArray) {
+	    return Arrays.asList(tArray);
+	}
+
+    }
+
+    private static MyCollections collections = new MyCollections();
+
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
+
+        // The Integer and String are the generics defining the object: it's not
+        // just a container, but a container of Integer and String.
 	public static Containers<Integer, String> getContainers() {
-		// TODO fix this function
-		return null;
+	    return collections;
 	}
 
 
 	public static void main(String[] args){
-		
+
 	}
 }
--- test/uchidb/ContainersTest.java
+++ test/uchidb/ContainersTest.java
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  */
 package uchidb;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 
 import java.util.List;
 import java.util.Map;
@@ -15,30 +15,30 @@ import org.junit.Test;
 
 /**
  * @author aelmore
- * Do not commit changes to this class 
+ * Do not commit changes to this class
  *
  */
 public class ContainersTest {
 
 	//Instantiated containers class
 	private Containers<Integer, String> cont;
-	
+
 	//Array of ints (primitive types)
 	//Uses shortcut syntax to make array
 	int[] intArray = {
 			1,14,4,20,14
 	};
-	
+
 	//Array of Integers (class type)
 	Integer[] intClassArray;
-	
+
 	//Array of Strings (class), with shortcut syntax
 	String[] strArray = {
 			"Uwe", "Eric","Vlaad","Sid","Richard", "Sean"
 	};
-	
-	
-	
+
+
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -52,8 +52,8 @@ public class ContainersTest {
 			// Putting primitive types into containers is primary reason why
 			intClassArray[i] = intArray[i];
 		}
-		
-		
+
+
 	}
 
 	@Test
@@ -62,8 +62,8 @@ public class ContainersTest {
 		assertTrue("Size of set is correct", 4 == s1.size());
 		assertTrue("Contains 4", s1.contains(intArray[2])); //Notice the autoboxing
 	}
-	
-	@Test 
+
+	@Test
 	public void testListCreation() {
 		List<Integer> l1 = cont.initList(intClassArray);
 		assertTrue("Size of set is correct", 5 == l1.size());
@@ -73,17 +73,17 @@ public class ContainersTest {
 		}
 	}
 
-	@Test 
+	@Test
 	public void testMapFunctionality(){
 		Map<String, Integer> myMap = cont.initEmptyMap();
-		
+
 		cont.storeMap(myMap);
 		for (int i =0; i < intArray.length; i++){
 			cont.addToMap(strArray[i], intClassArray[i], true);
 		}
 		boolean expFalse = cont.addToMap("Uwe", 1000, false);
 		boolean expTrue = cont.addToMap("Richard", new Integer(42), true);
-		
+
 		assertTrue("Map correct", intClassArray[2] == cont.getValueFromMap(strArray[2]));
 		assertTrue("Map correct", intClassArray[0] == cont.getValueFromMap(strArray[0]));
 		assertTrue("Map correct", intClassArray[0] != cont.getValueFromMap(strArray[1]));
@@ -92,15 +92,15 @@ public class ContainersTest {
 		assertTrue("Map correct", new Integer(-1) != cont.getValueFromMap(strArray[5], new Integer(-1)));
 		assertFalse(expFalse);
 		assertTrue(expTrue);
-		
+
 	}
 
-	@Test 
+	@Test
 	public void testContSingleton(){
 		Containers<Integer, String> cont2 = HW0Runner.getContainers();
 		assertTrue(cont == cont2);
 		assertTrue("Map correct", intClassArray[0] == cont2.getValueFromMap(strArray[0]));
-		
-		
+
+
 	}
 }
